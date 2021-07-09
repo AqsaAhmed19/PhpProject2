fe70f83a30984ac3ac6519c8d3419f10dfd87702
Aqsa KhaN <aqsakhan1906@gmail.com>
2021-07-09 21:13:47 +0500

    add tables in database

--- admin/test.sql
+++ admin/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2021 at 02:47 PM
+-- Generation Time: Jul 09, 2021 at 06:11 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `test`
+-- Database: `php_shopping_cart`
 --
 
 -- --------------------------------------------------------
@@ -125,7 +125,66 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`Id`, `Name`, `Files`, `Phone`, `City`, `Gender`) VALUES
-(1, 'Aqsa khan', 'img/060868fc822eb1cebaaa0aa33b13324a.png', 312348234, 'karachi', 'female');
+(2, 'Aqsa ', 'img/0036beef8bc1c24cfb923bac65d8f94b.jpg', 312348234, 'Faisalabad', 'female'),
+(12, 'Aqsa Ahmed', 'img/a1846377d1b493e7cc84654f17860275.jfif', 312348234, 'karachi', 'female'),
+(15, 'Ali Khan', 'img/5a7cb6dd01b9028399a871bb6e442fd8.jfif', 312348234, 'karachi', 'male'),
+(16, 'Taiba khalid', 'img/da28d955570bf222e0699c9fba1e7f53.jpg', 345676666, 'Sailkot', 'female'),
+(17, 'hassan', 'img/94c50767359823f353fd83c07d29c16e.png', 312348234, 'karachi', 'male'),
+(18, 'Aqsa Ahmed hj', 'img/0e55a58cf53560e81a49c09f7b3a5187.png', 312348234, 'karachi', 'female');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `admin_id` int(225) NOT NULL,
+  `admin_username` varchar(225) NOT NULL,
+  `admin_password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `order_id` int(225) DEFAULT NULL,
+  `order_name` varchar(225) NOT NULL,
+  `order_address` varchar(225) NOT NULL,
+  `order_email` varchar(225) NOT NULL,
+  `order_workphoneNo` int(225) NOT NULL,
+  `order_cellNo` int(225) NOT NULL,
+  `order_dob` date NOT NULL,
+  `order_category` varchar(225) NOT NULL,
+  `order_remarks` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `product_id` int(225) NOT NULL,
+  `product_name` varchar(225) NOT NULL,
+  `product_price` int(225) NOT NULL,
+  `product_qty` int(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -215,6 +274,24 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`Id`);
 
 --
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -252,7 +329,25 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `admin_id` int(225) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_id` int(225) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
